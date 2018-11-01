957548669ecc043fc8031d49d863cca3d7d5a8d3
Elmousski <a.moussier@hotmail.fr>
2018-11-01 22:26:27 +0100

    ajout du login

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,16 @@
 class StaticController < ApplicationController
 	def index
+	puts "********home => current_user??************"
+    puts @current_user
 	end
 	def contact
 	end
 	def team
 	end	
+
+  def not_connected
+  end
+
+  def invalid_login_password
+  end
 end
---
+++ test/controllers/registration_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class RegistrationControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get registration_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get registration_create_url
+    assert_response :success
+  end
+
+  test "should get logout" do
+    get registration_logout_url
+    assert_response :success
+  end
+
+end
